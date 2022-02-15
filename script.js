a7d9ce859436b9e9c564d54f0821b62ebc2b4bd4
Sandis <smanssandis@gmail.com>
2022-02-15 20:47:06 +0200

    Added slider that creates custom grid

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-let body = document.querySelector('body');
+// creates multiple divs to create a grid
+function createGrid() {
+    for (let i = 0; i < Math.pow(slider.value, 2); i++) {
+        container.appendChild(document.createElement('div'));
+    }
+    container.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
 
-let container = document.createElement('div');
+    // gets all the divs that create the grid
+    let divs = document.querySelectorAll('div.container > div');
 
-for (let i = 0; i < 256; i++) {
-    container.appendChild(document.createElement('div'));
+    // each div has an event listener that changes color when the mouse hovers over it
+    divs.forEach(div => {
+        div.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = 'purple');
+    });
 }
 
+// deletes the grid
+function deleteGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+// gets div by class name from html
+let main = document.querySelector('.main');
+
+// creates a new div 
+let container = document.createElement('div');
+
+// adds a class="container" to the div that contains all the divs 
 container.classList.add('container');
 
-body.appendChild(container);
+// adds created grid to the html
+main.appendChild(container);
 
-let divs = document.querySelectorAll('div.container > div');
+// gets div by class name from html
+let slider = document.querySelector('.slider');
+let value = document.querySelector('.value');
+let submit = document.querySelector('.submit');
 
-divs.forEach(div => {
-    div.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = 'purple');
-});
+// shows the value of the slider
+value.innerText = slider.value;
+
+// changes value text dynamically to the slider
+slider.addEventListener('input', () => {
+    value.innerText = slider.value;
+})
+
+// changes the grid layout by the slider number
+submit.addEventListener('click', (e) => {
+    deleteGrid();
+    createGrid();
+})
+
+createGrid();
